8cfce18aad2fdcc217d0b880d40563b75bcd99c8
Freyjadono <mike.stengel@gmail.com>
2014-05-12 23:19:00 -0400

    Added a gemfile Persitant data

--- bot.rb
+++ bot.rb
@@ -1,36 +1,54 @@
 require 'cinch'
+require 'cinch/plugins/identify'
+require 'yaml/store'
 
 
-$admin_room = '#TeamMetallurgy'
-$room = '#Metallurgy'
-
-$bot_name = 'MetallurgyBot'
-$server = 'localhost'
-
-$requests = {}
-
-
-class Request < Struct.new(:nick,:name, :channel)
+class Request < Struct.new(:nick, :name, :channel)
 
 end
 
+store = YAML::Store.new("data.yml")
+
+$admin_room = store.transaction { store.fetch(:admin_room, '#TeamMetallurgy') }
+$room = store.transaction { store.fetch(:room, :'#Metallurgy') }
+$bot_name = store.transaction { store.fetch(:bot_name, 'MetallurgyBot') }
+$server = store.transaction { store.fetch(:server_name, 'localhost') }
+
+$ns_name = store.transaction { store.fetch(:ns_name, "") }
+$ns_password = store.transaction { store.fetch(:ns_password, "") }
+
+$requests = {}
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.nick      = $bot_name
-    c.realname  = $bot_name
-    c.server    = $server
-    c.channels  = [$room, $admin_room]
+    c.nick = $bot_name
+    c.realname = $bot_name
+    c.server = $server
+    c.channels = [$room.to_s, $admin_room.to_s]
+
+    unless $ns_name.empty? && $ns_password.empty?
+      c.plugins.plugins = [Cinch::Plugins::Identify]
+      c.plugins.options[Cinch::Plugins::Identify] = {
+          :username => $ns_name,
+          :password => $ns_password,
+          :type => :nickserv,
+      }
+    end
   end
 
 
   on :message, /^!request (.+)/ do |m, minecraftname|
 
-    $requests[minecraftname] = Request.new(m.user.nick, minecraftname, m.channel)
+    $requests[minecraftname] = Request.new(m.user.nick, minecraftname, m.channel.name)
+
+    store.transaction do
+      request = store[:requests] || {}
+      request.merge! $requests
+    end
 
     Channel($admin_room).send "#{m.user.nick} has request to be white-listed with the name #{minecraftname}."
-    Channel($admin_room).send  "To accept type: !accept #{minecraftname}"
-    Channel($admin_room).send  "To deny type: !deny #{minecraftname}"
+    Channel($admin_room).send "To accept type: !accept #{minecraftname}"
+    Channel($admin_room).send "To deny type: !deny #{minecraftname}"
   end
 
   on :message, /^!accept (.+)/ do |m, minecraftname|
@@ -52,11 +70,13 @@ bot = Cinch::Bot.new do
   end
 
   on :message, /^!request-list/ do |m|
-    if $requests.empty?
+    requests = store.transaction { store[:requests] }
+
+    if requests.nil? || requests.empty?
       m.reply 'No pending white-list applications.'
     else
-      m.reply "#{$requests.size} Pending white-list applications:"
-      $requests.each_with_index { |(name, request), index| m.reply "#{index}: #{request.name}" }
+      m.reply "#{requests.size} Pending white-list applications:"
+      requests.each_with_index { |(name, request), index| m.reply "#{index}: #{request.name}" }
     end
   end
 end
